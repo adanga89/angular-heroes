f09db6a61271d985174bd26b06f0fc34a1d4354b
adanga89 <cuadrado015@gmail.com>
2022-05-26 16:55:38 -0500

    Servicio de heroes y Json-Server

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: [`
+  .container{
+    margin: 10px;
+  }
+  a {
+    text-decoration:none;
+  }
+  `]
 })
 export class HomeComponent implements OnInit {
 
