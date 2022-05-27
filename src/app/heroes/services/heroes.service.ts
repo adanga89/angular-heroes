8fbf7d9476fbe61f03daf4c85d9eb846cb0b7389
adanga89 <cuadrado015@gmail.com>
2022-05-26 19:35:48 -0500

    BaseURL en environment

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Hero } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]>{
-    return this.http.get<Hero[]>("http://localhost:3000/heroes");
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroeById(id: string): Observable<Hero>{
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`);
   }
 }
