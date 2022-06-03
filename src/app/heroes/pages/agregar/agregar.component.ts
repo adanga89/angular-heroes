5759b0b7be0d5cd8c7318e87fc4f73ae0d8b2b14
adanga89 <cuadrado015@gmail.com>
2022-06-03 13:18:19 -0500

    agregando url de imagen Heroe | editar

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -7,7 +7,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 5px;
+    }
+  `
   ]
 })
 export class AgregarComponent implements OnInit {
@@ -38,11 +43,13 @@ export class AgregarComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params
-    .pipe(
-      switchMap(({id})=> this.heroeService.getHeroeById(id))
-    )
-    .subscribe( heroe => this.heroe = heroe);
+    if(this.router.url.includes("editar")){
+      this.activatedRoute.params
+      .pipe(
+        switchMap(({id})=> this.heroeService.getHeroeById(id))
+      )
+      .subscribe( heroe => this.heroe = heroe);
+    }
   }
 
   guardar(){
--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -7,6 +7,11 @@ import { Hero } from '../interfaces/heroes.interface';
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Hero): string {
+    if(!heroe.id && heroe.alt_img)
+      return `assets/no-image.png`;
+    else if(heroe.alt_img) 
+      return heroe.alt_img;
+
     return `assets/heroes/${ heroe.id }.jpg`;
   }
 
